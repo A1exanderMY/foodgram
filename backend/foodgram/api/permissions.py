78e55af5a099fe82449407fcd86e2c5322ea1c78
Alexander Myshkin <un_forastero@outlook.com>
2024-08-07 10:13:48 +0300

    fifth

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -3,15 +3,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAuthorOrAdminOrReadOnly(BasePermission):
 
-    def has_permission(self, request, view):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
         return (
             obj.author == request.user
-            or request.user.is_superuser
-            or request.user.is_staff
+            or request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_superuser
         )
